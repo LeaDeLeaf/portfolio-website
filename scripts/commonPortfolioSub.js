74f8c7c9403bf69a6b6135ad5b2dcea4a54389c8
LeaDeLeaf <104687952+LeaDeLeaf@users.noreply.github.com>
2023-05-28 00:40:17 +0800

    Add imgs & vids with description tooltips

---
+++ scripts/commonPortfolioSub.js
@@ -0,0 +1,241 @@
+// Hotel Management App
+tippy('#hotelmanagement1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Hotel Management Class (Main Program)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Helper Class (Helps in gaining Input from user)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Reservation Class (Aids in gaining info about reservations)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement4', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Hotel Management App File Structure</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement5', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Room Class (Aids in getting info about rooms)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement6', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Admin Class (Aids in getting info about admins in the app)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelmanagement7', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Member Class (Aids in getting info about members in the app)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+
+// World Xplore
+tippy('#worldxplore1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Full Demonstration of website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#worldxplore2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Contact Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#worldxplore3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Home Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#worldxplore4', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Rediscover Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+
+// Hotel Review Website
+tippy('#hotelreview1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Full Demo of the website (Playing at x3 speed)</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelreview2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Login Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelreview3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Hotel List Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelreview4', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Database of the website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#hotelreview5', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>File Structure of the website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+// Covid Website
+tippy('#covid1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Full Demonstration of website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#covid2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Number of Cases Section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#covid3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Vaccination Status Section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#covid4', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Location of Vaccination Centres Section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+
+// SOI Website
+tippy('#soi1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Full Demonstration of website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#soi2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Navbar and Banner Section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#soi3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Featured Section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+
+// Exercise Tracker Website
+tippy('#exercisetracker1', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Full Demonstration of website</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#exercisetracker2', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Login Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#exercisetracker3', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Different Levels Page</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#exercisetracker4', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Input hours per day section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#exercisetracker5', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Success in reaching goal section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
+
+tippy('#exercisetracker6', {
+    placement: 'bottom',
+    allowHTML: true,
+    content: "<center>Failure in reaching goal section</center>",
+    animation: 'scale-extreme',
+    arrow: false,
+});
