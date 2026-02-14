59280b9e0458118a8313841ac1a261877cf1df70
Lea Gabay <lea101004@gmail.com>
2026-02-14 20:44:58 +0800

    Remove .jsx extensions from imports for build

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Projects from './pages/Projects.jsx'
-import Contact from './pages/Contact.jsx'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 import './app.css'
 
 function App() {
